0484344f951a30e67b568fadb7aa1ca3bce2618f
ilgner becheleni <ilgner.gui@gmail.com>
2023-03-26 20:58:16 -0300

    cors swagger

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ const pool = new Pool({
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: 'http://127.0.0.1:5500'
+    origin: ['http://127.0.0.1:5500','http://localhost']
   }));
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
